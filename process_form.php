17196a1fddea29c5f69e888b9eb9ca74b3df3391
anuseelan21 <144528887+anuseelan21@users.noreply.github.com>
2024-06-01 21:52:23 +0530

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -5,7 +5,7 @@ $username = "admin";
 $password = "12345678";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+$conn = new mysqli($servername, $username, $password,$dbname,3309);
 
 // Check connection
 if ($conn->connect_error) {
