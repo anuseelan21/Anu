9bbb5048f133e3a88952e6233a86a8d5c237ce1b
anuseelan21 <144528887+anuseelan21@users.noreply.github.com>
2024-06-01 22:07:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 	<?php
-		include 'includes/conn.php';				// Fetch data from database
-$sql = "SELECT id, name, email, message, created_at FROM contacts";
+		require_once 'includes/conn.php';				// Fetch data from database
+$sql = "SELECT id FROM sales";
 $result = mysqli_query($database, $sql);
 
 if (mysqli_num_rows($result) > 0) {
@@ -8,19 +8,11 @@ if (mysqli_num_rows($result) > 0) {
     echo "<table border='1'>
             <tr>
                 <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Message</th>
-                <th>Date</th>
             </tr>";
     // Output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         echo "<tr>
                 <td>{$row['id']}</td>
-                <td>{$row['name']}</td>
-                <td>{$row['email']}</td>
-                <td>{$row['message']}</td>
-                <td>{$row['created_at']}</td>
               </tr>";
     }
     echo "</table>";
