822e4891e8b3df24a22311a30170ce5b38d5a4a0
anuseelan21 <144528887+anuseelan21@users.noreply.github.com>
2024-06-01 17:57:19 +0530

    Delete Reorder directory

--- Reorder/test.py
+++
@@ -1,4 +0,0 @@
-line = "  DJI_RIGHT_20240228102328_0018_V.JPG - ID: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20"
-filename = line.split('_', 2)[2].split(' - ')[0]
-
-print(filename)
