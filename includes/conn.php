be190ad0809ffbb1bb6958322464a557b5747eee
anuseelan21 <anuseelan786@gmail.com>
2024-06-01 23:36:38 +0530

    anu db

--- includes/conn.php
+++ includes/conn.php
@@ -1,30 +1,15 @@
 <?php
+$servername = "bcas-db.cb8ygmqiui6l.us-east-1.rds.amazonaws.com";
+$dbname="ecomm";
+$username = "admin";
+$password = "12345678";
 
-Class Database{
- 
-	private $server = "mysql:host=localhost;dbname=ecomm";
-	private $username = "root";
-	private $password = "";
-	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
-	protected $conn;
- 	
-	public function open(){
- 		try{
- 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
- 			return $this->conn;
- 		}
- 		catch (PDOException $e){
- 			echo "There is some problem in connection: " . $e->getMessage();
- 		}
- 
-    }
- 
-	public function close(){
-   		$this->conn = null;
- 	}
- 
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname,3309);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-
-$pdo = new Database();
- 
+echo "Connected successfully";
 ?>
